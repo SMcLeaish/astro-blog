15be5a7e12c8b577d9d9cc2b25c6047faabe36e5
Sean McLeaish <smcleaish@gmail.com>
2025-02-19 17:27:35 -0500

    Took out preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,8 @@ import { defineConfig } from "astro/config";
 
 import alpinejs from "@astrojs/alpinejs";
 
-import preact from "@astrojs/preact";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://seanmcleaish.com",
-  integrations: [alpinejs({ entrypoint: "/src/entrypoint" }), preact()],
+  integrations: [alpinejs({ entrypoint: "/src/entrypoint" })],
 });
-
